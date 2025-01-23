fbd79e82aa37b009f98ad5322c86f96fd4a64dc7
VALTIEL777 <elianmedinacobos@gmail.com>
2025-01-22 20:02:29 -0600

    fix: modificar archivos de connfiguracion de proyecto

--- Multi-platformAppMobile/src/app/app-routing.module.ts
+++ Multi-platformAppMobile/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'login',
+    loadChildren: () => import('./features/auth/login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
