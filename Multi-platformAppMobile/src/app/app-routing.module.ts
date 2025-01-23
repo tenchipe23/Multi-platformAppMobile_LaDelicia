e82eff90c12c16dcc0c4e497163241c0bfd2f71e
VALTIEL777 <elianmedinacobos@gmail.com>
2025-01-22 20:36:01 -0600

    fix: archivo de rutas

--- Multi-platformAppMobile/src/app/app-routing.module.ts
+++ Multi-platformAppMobile/src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'login',
+    loadChildren: () => import('./features/auth/login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
-  },  {
-    path: 'login',
-    loadChildren: () => import('./features/auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./features/auth/register/register.module').then( m => m.RegisterPageModule)
   },
 
 ];
