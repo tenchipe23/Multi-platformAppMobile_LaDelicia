d8260579d1bdf0cf46787bc3dd6f3d481956b611
Roberto-H-Z <20233l001058@gmail.com>
2025-02-04 19:17:36 -0600

    fead: agregar funcionalidades al carrito para validar si el usuario ya tiene productos en el carrito o no

--- Multi-platformAppMobile/src/app/features/carrito/carrito.page.ts
+++ Multi-platformAppMobile/src/app/features/carrito/carrito.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../core/services/cart.service'; // Importa el servicio CartService
+import { ToastController } from '@ionic/angular'; // Para mostrar notificaciones
 
 @Component({
   selector: 'app-carrito',
@@ -8,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoPage implements OnInit {
 
-  constructor() { }
+  
+  carrito: any[] = []; // Lista de productos en el carrito
+
+  constructor(private cartService: CartService, private toastController: ToastController) {}
 
   ngOnInit() {
+    this.carrito = this.cartService.obtenerCarrito(); // Obtén los productos del carrito
   }
 
+   // Método para calcular el total del carrito
+   get total(): number {
+    return this.carrito.reduce((total, item) => total + (item.price_product * item.quantity), 0);
+  }
+
+  eliminarDelCarrito(item: any) {
+    this.carrito = this.carrito.filter(producto => producto !== item);
+    this.cartService.actualizarCarrito(this.carrito);
+    this.mostrarToast(`"${item.name_product}" eliminado del carrito correctamente`);
+  }
+
+  async mostrarToast(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000,
+      color: 'danger',
+      position: 'bottom'
+    });
+
+    await toast.present();
+  }
 }
